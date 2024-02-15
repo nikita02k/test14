13920ab02632305a33422ec7c9d81b3b6b195785
nikita02k <gritskevich-nikita@mail.ru>
2024-02-15 14:26:15 +0700

    to9 hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 int main() {
 string name;
-std::cout << "your name:";
+std::cout << "your name:";//
 std::cin >> name
 std::cout << "hello world from @" << name << endl;
 return 0;
