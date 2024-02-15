4e3b25597c38bcc1a2b0986e6861a94d5e023935
nikita02k <gritskevich-nikita@mail.ru>
2024-02-15 13:25:38 +0700

    one4 hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+int main() {
+string name;
+std::cout << "your name:";
+std::cin >> name
+std::cout << "hello world from @" << name << endl;
+return 0;
+        }
